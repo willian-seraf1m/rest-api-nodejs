9873239ed41858384ce8bb166d85bae62ef6c49d
willian <willianserafim2017@gmail.com>
2025-08-11 14:59:22 -0300

    Automated Testing - Testing Transaction Creation

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,9 @@
-import fastify from 'fastify'
-import { knex } from './database'
-
-const app = fastify()
-
-app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
-})
+import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Http Server Running')
